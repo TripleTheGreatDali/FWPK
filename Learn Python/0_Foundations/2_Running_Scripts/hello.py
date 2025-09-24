877767c2812289d32d1cdc9eb2b054316d8822f6
MD FOYSAL AHMED <64484441+TripleTheGreatDali@users.noreply.github.com>
2025-09-24 13:04:29 +0600

    updates

---
+++ Learn Python/0_Foundations/2_Running_Scripts/hello.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+
+# Red text
+print("\033[91mHello, World!\033[0m")
+
+# Green text
+print("\033[92mHello, World!\033[0m")
+
+# Bold and Blue text
+print("\033[1;94mHello, World!\033[0m")
+# Underlined and Yellow text
+print("\033[4;93mHello, World!\033[0m")
